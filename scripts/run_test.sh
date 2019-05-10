aa08cafe8f8e891ccc6fcaba0af4cd919d043d79
rannk <rannk@163.com>
2019-05-10 13:52:20 +0800

    u

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH = 'deploy' ] then
+if [ $TRAVIS_BRANCH = "deploy" ]; then
 docker pull aerokube/selenoid:latest-release
 docker pull selenoid/vnc_chrome:66.0
 docker run -d  --name selenoid  -p 4444:4444  -v /var/run/docker.sock:/var/run/docker.sock    -v `pwd`/config/:/etc/selenoid/:ro  aerokube/selenoid:latest-release  --conf /etc/selenoid/browsers.json -timeout 70s
