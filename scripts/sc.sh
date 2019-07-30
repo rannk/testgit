c04af82d3997c3f5a8fb071ff12c5c3d5459849d
rannk <rannk@163.com>
2019-07-30 16:20:23 +0800

    u

--- scripts/sc.sh
+++ scripts/sc.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #export REG_TEST_DIR=$REG_TEST_DIR
-if [ ! $MY_DIR ]; then
-    DIR = test
-else
-    DIR=$MY_DIR
-fi
-echo $DIR
+    if [ ! $REG_TEST_DIR ];then
+        TEST_DIR=`pwd`/test/
+    else
+        if [ $REG_TEST_DIR = /* ]; then
+            TEST_DIR=$REG_TEST_DIR
+        else
+            TEST_DIR=`pwd`/$REG_TEST_DIR
+    fi
+echo $TEST_DIR
